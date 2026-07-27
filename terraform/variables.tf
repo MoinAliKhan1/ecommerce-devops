2761f1dfc95ba389013dc7fff7c5771647820e27
Moin Ali Khan <moinalikhan97.ma@gmail.com>
2026-07-27 14:54:58 +0000

    Added VPC networking with subnets, internet gateway and route tables

---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "public_subnet_1_cidr" {
+  description = "Public Subnet 1 CIDR"
+  type        = string
+}
+
+variable "public_subnet_2_cidr" {
+  description = "Public Subnet 2 CIDR"
+  type        = string
+}
+variable "private_subnet_1_cidr" {
+  description = "Private Subnet 1 CIDR"
+  type        = string
+}
+
+variable "private_subnet_2_cidr" {
+  description = "Private Subnet 2 CIDR"
+  type        = string
+}
